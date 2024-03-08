68eb0073adc4a9e7df4d1991ba3a1b9e1e8bfff4
chanhwi <chanhwi0202@naver.com>
2024-03-08 09:48:23 +0900

    1주차 과제

--- AI_1주차.py
+++ AI_1주차.py
@@ -1,2 +1,16 @@
 Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
+>>> 100
+100
+>>> 10 + 20
+30
+>>> 10 - 20
+-10
+>>> 10 * 20
+200
+>>> 10 / 20
+0.5
+>>> 123 * 8765
+1078095
+>>> 10 + 20 * 30
+610
